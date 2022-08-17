813154196c59a6a6a63773ef4d5026b12980f093
노호준 <hh950131@gmail.com>
2022-08-17 20:53:30 +0900

    solve P8393

---
+++ BOJ/P8393.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << n * (n + 1) / 2;
+
+    return 0;
+}
