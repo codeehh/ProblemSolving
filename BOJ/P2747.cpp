12c3c242fbe56f346a16aa03874fd5413fa7d1b2
노호준 <hh950131@gmail.com>
2022-09-14 21:50:40 +0900

    solve P2747

---
+++ BOJ/P2747.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    long long fib[46];
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    cout << fib[n];
+
+    return 0;
+}
