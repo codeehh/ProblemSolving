d8cce8b8782bd6e189cf1c961176c041b5b5d118
노호준 <hh950131@gmail.com>
2022-09-15 09:45:07 +0900

    solve P10953

---
+++ BOJ/P10953.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int a, b;
+        char c;
+        cin >> a >> c >> b;
+        cout << a + b << '\n';
+    }
+
+    return 0;
+}
