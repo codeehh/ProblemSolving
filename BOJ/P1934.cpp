063f86dfb8bbd9ae555ade3a377ff60fd4c0fa2a
노호준 <hh950131@gmail.com>
2022-09-17 11:37:17 +0900

    solve P1934

---
+++ BOJ/P1934.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << a * b / gcd(a, b) << '\n';
+    }
+
+    return 0;
+}
