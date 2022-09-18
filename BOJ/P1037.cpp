e3792ed5361606db936a17d02093655e325ab156
노호준 <hh950131@gmail.com>
2022-09-18 19:38:41 +0900

    solve P1037

---
+++ BOJ/P1037.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    int arr[50];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    cout << arr[0] * arr[n - 1];
+
+    return 0;
+}
