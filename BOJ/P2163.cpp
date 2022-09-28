0c5ec11cc699ab55dcc49713ec7b477591381303
노호준 <hh950131@gmail.com>
2022-09-29 00:27:40 +0900

    solve P2163

---
+++ BOJ/P2163.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    cout << n * m - 1;
+
+    return 0;
+}
