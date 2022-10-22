6ca94f812763119ae447984a8468e14e1d30f949
노호준 <hh950131@gmail.com>
2022-10-22 17:14:57 +0900

    solve P5717

---
+++ BOJ/P5717.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while (true) {
+        int a, b;
+        cin >> a >> b;
+        if (a == 0 && b == 0)
+            break;
+        cout << a + b << '\n';
+    }
+
+    return 0;
+}
