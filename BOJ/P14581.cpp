b4b9c18e155fb443c2f23f95f21f1a53a59bec6f
노호준 <hh950131@gmail.com>
2022-11-16 23:55:33 +0900

    solve P14581

---
+++ BOJ/P14581.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string str;
+    cin >> str;
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << str << "::fan:\n";
+    cout << ":fan::fan::fan:\n";
+    return 0;
+}
