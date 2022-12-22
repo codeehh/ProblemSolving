5a530954cf3ea36f59cf2425e266836181164c03
노호준 <hh950131@gmail.com>
2022-12-22 20:43:32 +0900

    solve P10162

---
+++ BOJ/P10162.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    if (t % 10 != 0)
+        cout << -1;
+    else {
+        cout << t / 300 << " ";
+        t %= 300;
+        cout << t / 60 << " ";
+        t %= 60;
+        cout << t / 10;
+    }
+
+    return 0;
+}
